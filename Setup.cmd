4c22b287513ebc9d3ffdbb42080424d4db1ae540
nikhil.leo <l00170961@atu.ie>
2023-01-01 11:07:48 +0000

    Created New Repository And Added Readme File

---
+++ Setup.cmd
@@ -0,0 +1,7 @@
+echo "# PowerShell" >> README.md
+git init
+git add README.md
+git commit -m "setup commit"
+git branch -M main
+git remote add origin https://github.com/nikhilleo07/PowerShell.git
+git push -u origin main
