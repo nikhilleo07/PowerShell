7f8e55a0167b5d31b659fe6ab75859c686d37de0
nikhil.leo <l00170961@atu.ie>
2023-01-01 11:11:05 +0000

    Corrected and added Get Variable function to the script to find TAX

--- Exercise 2/Tax calculation.ps1
+++ Exercise 2/Tax calculation.ps1
@@ -8,4 +8,5 @@ $dir_listing = Get-ChildItem M:\Cloud\PowerShell
 $dir_listing
 test-path variable:\dir_listing
 New-Variable JORzVariable -value 3.142 -description "PI with write-protection" -option ReadOnly
+Get-Variable JORzVariable
 
